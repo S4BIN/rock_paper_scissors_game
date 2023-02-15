b8c5bf99d9a5fdb6ae4c07ee8dfe1785d0643146
Burlacu Ioan Sabin <118127389+S4BIN@users.noreply.github.com>
2023-02-15 18:17:08 +0200

    index.html and script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function playRound(playerSelection, computerSelection) {
+    // your code here!
+    return("You lose")
+  }
+   
+  const playerSelection = "rock";
+  const computerSelection = getComputerChoice();
+  console.log(playRound(playerSelection, computerSelection));
