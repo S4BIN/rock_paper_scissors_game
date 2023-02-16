a324fe4908fa758620e2ac26de597d795f98b87e
Burlacu Ioan Sabin <118127389+S4BIN@users.noreply.github.com>
2023-02-16 16:34:54 +0200

    index.html and script.js

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
-function playRound(playerSelection, computerSelection) {
-    // your code here!
-    return("You lose")
+function startGame() {
+  cm = Math.floor(Math.random() * 3);
+
+  input = prompt("Enter your Choice!");
+
+  pm = parseInt(input);
+
+if (cm == 0&& pm == 1) {
+    alert("You beat Computer with Paper against Rock");
+  } else if ((cm = 0&& pm == 2)) {
+    alert("Computer Beat you with Rock");
+  } else if ((cm = 1&& pm == 0)) {
+    alert("Computer Beat you With Paper");
+  } else if ((cm = 1&& pm == 2)) {
+    alert("You beat computer with Scissor against Paper");
+  } else if ((cm = 2&& pm == 0)) {
+    alert("You beat computer with Rock against Scissor");
+  } else if ((cm = 2&& pm == 1)) {
+    alert("Computer Beat you With Scissor");
+  } else {
+    alert("It's a tie");
   }
-   
-  const playerSelection = "rock";
-  const computerSelection = getComputerChoice();
-  console.log(playRound(playerSelection, computerSelection));
+}
